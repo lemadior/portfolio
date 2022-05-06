c62de9382aca0e753db27106bb2bfe41d2f92663
lemadior <admin@local.net>
2022-05-06 14:36:08 +0300

    Final Release

--- new/js/main.js
+++ new/js/main.js
@@ -5,19 +5,20 @@ window.onload = function() {
     let copyright = document.getElementById('copyright');
     copyright.innerHTML = "Copyright &copy; " + (new Date()).getFullYear() + " | ";
 
-    let currImage = null;
-    let currSlider = null;
+    let currImage = null;   // Current image in the slider
+    let currSlider = null;  // Current slider itself
     let img = null;
   
-    let expand = null;
-    let collapse = null;
-    let content = null;
-    let span = null;
+    let expand = null;      // Button for expand information blok within slider
+    let collapse = null;    // Button for collapse information blok within slider
+    let content = null;     // Content area for the information blok within slider
+    let span = null;        // Header of content area for the information blok within slider
 
     let slideIndex = 0;
     let timer = null;
 
-    let hover_enable = true;
+    let hover_enable = true; // Flag changes behaviour of hover effect for 'circle' buttons
+
     if (window.innerWidth <= 839) {
         hover_enable = false;
     }
@@ -46,29 +47,34 @@ window.onload = function() {
 
     function showSlides() {
 
-        let i;
+        let i = null;
         let slides = document.getElementsByClassName("image-sliderfade");
         let dots = document.getElementsByClassName("dot");
         
         for (i=0; i < slides.length; i++) {
+
             if (slides[i].classList.contains('hide')) {
                 slides[i].classList.remove('hide');
             }
+
             slides[i].style.display = 'none';
+
         }
 
         if (expand !== null) {
             expand.removeEventListener('click', onExpand);
         }
+
         if (collapse !== null) {
             collapse.removeEventListener('click', onCollapse);
         }
+
         if (currSlider !== null) {
             currSlider.classList.toggle('hide');
         }
 
         slideIndex++;
-        //console.log(slideIndex);
+
         if (slideIndex > slides.length) {
             slideIndex = 1;
         }
@@ -80,18 +86,24 @@ window.onload = function() {
         currSlider.classList.toggle('hide');
         img = currImage.children[1];
 
-      span = currSlider.children[0];
+        span = currSlider.children[0];
 
         for (let i=0;i<currSlider.childElementCount;i++) {
         
             if (currSlider.children[i].className.includes('expand')) {
-                 expand=currSlider.children[i];
-                 expand.addEventListener('click', onExpand);
+                
+                expand=currSlider.children[i];
+                expand.addEventListener('click', onExpand);
+            
             } else if (currSlider.children[i].className.includes('collapse')) {
-                 collapse = currSlider.children[i];
-                 collapse.addEventListener('click', onCollapse);
+                
+                collapse = currSlider.children[i];
+                collapse.addEventListener('click', onCollapse);
+            
             } else if (currSlider.children[i].className.includes('content')) {
-                 content = currSlider.children[i];
+                
+                content = currSlider.children[i];
+            
             } 
 
          }
@@ -100,69 +112,86 @@ window.onload = function() {
             dots[i].className = dots[i].className.replace(" active", "");
         }
 
-      //  slides[slideIndex-1].style.display = "flex";
         dots[slideIndex-1].className += " active";
 
         timer = setTimeout(showSlides, 5000);
+    
     }
 
     function stopSlides() {
+        
         if (timer === null) {
             return;
         }
         
         clearTimeout(timer);
+        
         timer = null;
+        
         slideIndex--;
+    
     }
 
     function dotClick() {
+    
         let num = this.id.replace('dot','');
+        
         if (expand.className.includes('hide')) {
             onCollapse();
         }
+
         stopSlides();
+        
         slideIndex = num-1;
+        
         showSlides();
+    
     }
 
     function rarrowClick() {
-        console.log('rarrow');
+
         if (expand.className.includes('hide')) {
             onCollapse();
         }
+
         stopSlides();
+
         slideIndex++;
+
         showSlides();
+    
     }
 
     function larrowClick() {
+        
         if (expand.className.includes('hide')) {
             onCollapse();
         }
+        
         stopSlides();
         slideIndex--;
 
         if (slideIndex < 0) slideIndex = 3;
 
         showSlides();
+    
     }
 
     function setSliderHeight() {
+    
         if (!expand.className.includes('hide')) {
-            console.log('expand-c');
             currSlider.style.height = '';
             currSlider.style.marginBottom = '';
         } else {
-            console.log('collapse-c');
             let height = img.offsetHeight;
             currSlider.style.height = height+"px";
             currSlider.style.marginBottom = -height+"px";
         }
+    
     }
 
     function onExpand() {
-        console.log('expand');
+        
         span.classList.toggle('hide');
         expand.classList.toggle('hide');
         collapse.classList.toggle('hide');
@@ -170,9 +199,13 @@ window.onload = function() {
         currImage.classList.add('sbtn__collapse');
         currSlider.classList.remove('sl__expand');
         currSlider.classList.add('sl__collapse');
+        
         setSliderHeight();
+        
         content.classList.toggle('hide');
+
         stopSlides();
+    
     }
 
     function onCollapse() {
@@ -184,11 +217,18 @@ window.onload = function() {
         currImage.classList.remove('sbtn__collapse');
         currSlider.classList.add('sl__expand');
         currSlider.classList.remove('sl__collapse');
+        
         setSliderHeight();
+        
         content.classList.toggle('hide');
-        // slideIndex--;
+
         showSlides();
     }
+
+    larrow.addEventListener('click', larrowClick);
+    rarrow.addEventListener('click', rarrowClick);
+
+    /* Protfolio part */
 
     function onMouseEnter(num) {
 
@@ -221,135 +261,131 @@ window.onload = function() {
  
     }
 
-    larrow.addEventListener('click', larrowClick);
-    rarrow.addEventListener('click', rarrowClick);
+    function showProject(project) {
+        
+        project.classList.toggle('hide');
+        
+        if (!hover_enable) {
+            project.children[2].classList.toggle('hide');
+        }
+
+    }
+
+    function hideProject(project) {
+        
+        project.classList.toggle('hide');
+        
+        if (!hover_enable) {
+            project.children[2].classList.toggle('hide');
+        }
+
+    }
+
+    project1.onmouseenter = function(event) {
+        
+        if (hover_enable) {
+            onMouseEnter(0);
+        }
+
+    };
+
+    project1.onmouseleave = function(event) {
+        
+        if (hover_enable) {
+            onMouseLeave(0);
+        }  
+
+    };
+
+    project1.onclick = function(event) {
+        
+        if (hover_enable) {
+            window.open("https://lemadior.pp.ua", "_blank");
+        } else {
+            onMouseEnter(0);
+        }
+
+    }
+
+    close1.onclick = function(event) {
+        
+        onMouseLeave(0)
+    
+    }
+
+    project2.onmouseenter = function(event) {
+        
+        if (hover_enable) {
+            onMouseEnter(1);
+        }
+    };
+
+    project2.onmouseleave = function(event) {
+
+        if (hover_enable) {
+            onMouseLeave(1);
+        }
+
+    };
+
+    project2.onclick = function(event) {
+        
+        if (hover_enable) {
+            window.open("https://lemadior.pp.ua/nikos/start.html", "_blank");
+        } else {
+            onMouseEnter(1);
+        }  
+    
+    }
+
+    close2.onclick = function(event) {
+        
+        onMouseLeave(1)
+    
+    }
+
+    project3.onmouseenter = function(event) {
+ 
+        if (hover_enable) {
+            onMouseEnter(2);
+        }
+
+    };
+
+    project3.onmouseleave = function(event) {
+
+        if (hover_enable) onMouseLeave(2);
+
+    };
+
+    project3.onclick = function(event) {
+        
+        if (hover_enable) {
+            window.open("https://olliplus.com.ua", "_blank");
+        } else {
+            onMouseEnter(2);
+        }
+
+    }
+
+    close3.onclick = function(event) {
+
+        onMouseLeave(2)
+
+    }
+
+    /* Global section */
 
     window.addEventListener('resize', function(event){
+        
         if (window.innerWidth <= 839) {
             hover_enable = false;
         } else {
             hover_enable = true;
         }
+        
         setSliderHeight();
+
     });
-
-    /* Protfolio part */
-
-    function showProject(project) {
-        project.classList.toggle('hide');
-        if (!hover_enable) project.children[2].classList.toggle('hide');
-    }
-
-    function hideProject(project) {
-        project.classList.toggle('hide');
-        if (!hover_enable) project.children[2].classList.toggle('hide');
-    }
-
-    project1.onmouseenter = function(event) {
-        
-        console.log(hover_enable);
-        if (hover_enable) onMouseEnter(0);
-        
-    };
-
-    project1.onmouseleave = function(event) {
-        
-        if (hover_enable) onMouseLeave(0)
-       
-    };
-
-    project1.onclick = function(event) {
-        if (hover_enable) {
-            window.open("https://lemadior.pp.ua", "_blank");
-        } else {
-            // console.log(window.innerWidth);
-            // modal.classList.toggle('premodal');     
-            // modal.classList.toggle('modal');
-            // document.body.style.overflow="hidden";
-
-            // viewProject.classList.toggle('hide');
-            // viewProject.classList.remove('view_project');
-            // viewProject.classList.toggle('hide');
-            // let project = viewProject.children[0];
-            // showProject(project);
-            onMouseEnter(0);
-        }
-    }
-
-    close1.onclick = function(event) {
-        // modal.classList.toggle('premodal');     
-        // modal.classList.toggle('modal');
-        // document.body.style.removeProperty('overflow');
-        // viewProject.classList.remove('view_project');
-        // viewProject.classList.add('view_project_modal');
-        // let project = viewProject.children[0];
-        // hideProject(project);
-        // viewProject.classList.toggle('hide');
-        onMouseLeave(0)
-    }
-
-    project2.onmouseenter = function(event) {
-        console.log('Project 2 IN')
-        if (!hover_enable) return;
-        // viewProject.classList.toggle('hide');
-        // let project = viewProject.children[1];
-        // showProject(project);
-        onMouseEnter(1);
-    };
-
-    project2.onmouseleave = function(event) {
-        console.log('Project 2 OUT')
-        if (!hover_enable) return;
-        // let project = viewProject.children[1];
-        // hideProject(project);
-        // viewProject.classList.toggle('hide');
-        onMouseLeave(1);
-    };
-
-    project2.onclick = function(event) {
-        if (hover_enable) {
-            window.open("https://lemadior.pp.ua/nikos/start.html", "_blank");
-        } else {
-            console.log(window.innerWidth);
-            onMouseEnter(1);
-        }
-        
-    }
-
-    close2.onclick = function(event) {
-        onMouseLeave(1)
-    }
-
-    project3.onmouseenter = function(event) {
-        console.log('Project 3 IN');
-        if (!hover_enable) return;
-        // viewProject.classList.toggle('hide');
-        // let project = viewProject.children[2];
-        // showProject(project);
-        onMouseEnter(2);
-    };
-
-    project3.onmouseleave = function(event) {
-        console.log('Project 3 OUT');
-        if (!hover_enable) return;
-        // let project = viewProject.children[2];
-        // hideProject(project);
-        // viewProject.classList.toggle('hide');
-        onMouseLeave(2);
-    };
-
-    project3.onclick = function(event) {
-        if (hover_enable) {
-            window.open("https://olliplus.com.ua", "_blank");
-        } else {
-            console.log(window.innerWidth);
-            onMouseEnter(2);
-        }
-        
-    }
-
-    close3.onclick = function(event) {
-        onMouseLeave(2)
-    }
 
 }
