d6797bcbdc25b1b0c172713effb085c6d8506ad3
lemadior <lemadioro@gmail.com>
2024-04-16 10:30:10 +0300

    Cleanup html-structure, css and JavaScript.

--- js/main.js
+++ js/main.js
@@ -1,223 +1,204 @@
 'use strict';
 
 window.onload = function() {
+    // Do nothing if we're not on the home page
+    if (document.title.includes('comments')) {
+        return;
+    }
+
     const copyright = document.getElementById('copyright');
     copyright.innerHTML = "Copyright &copy; " + (new Date()).getFullYear() + " ";
 
-    let currImage = null;   // Current image in the slider
-    let currSlider = null;  // Current slider itself
-    let img = null;
-  
-    let expand = null;      // Button for expand information blok within slider
-    let collapse = null;    // Button for collapse information blok within slider
-    let content = null;     // Content area for the information blok within slider
-    let span = null;        // Header of content area for the information blok within slider
-
-    let slideIndex = 0;
-    let timer = null;
-
-    let hover_enable = true; // Flag changes behaviour of hover effect for 'circle' buttons
-
-    if (window.innerWidth <= 839) {
-        hover_enable = false;
+    const projectLinks = {
+        project1: "https://lemadior.pp.ua",
+        project2: "https://lemadior.pp.ua/nikos/start.html",
+        project3: "https://olliplus.com.ua"
     }
 
-    let larrow = document.getElementById('arrow-left');
-    let rarrow = document.getElementById('arrow-right');
-
-    let dots =  document.getElementsByClassName("dot");
-
-    let project1 = document.getElementById('first__project');
-    let project2 = document.getElementById('second__project');
-    let project3 = document.getElementById('third__project');
-
-    let modal = document.getElementsByClassName("premodal")[0];;
-    let viewProject = document.getElementsByClassName("view_project")[0];
-
-    let close1 = document.getElementById('close1');
-    let close2 = document.getElementById('close2');
-    let close3 = document.getElementById('close3');
-
-    for (let dot = 0; dot < dots.length; dot++) {
-        dots[dot].addEventListener('click', dotClick);
+    const slider = {
+    	image: null,
+    	current: null,
+    	expand: null,
+    	collapse: null,
+    	content: null,
+    	img: null,
+    	span: null,
+    	index: 0,
+    	timer: null
     }
+
+    const lArrow = document.getElementById('arrow-left');
+    const rArrow = document.getElementById('arrow-right');
+
+    const dots =  Array.prototype.slice.call(document.getElementsByClassName("dot"));
+
+    const project1 = document.getElementById('first__project');
+    const project2 = document.getElementById('second__project');
+    const project3 = document.getElementById('third__project');
+
+    const modal = document.getElementsByClassName("premodal")[0];
+    const viewProject = document.getElementsByClassName("view_project")[0];
+
+    const close1 = document.getElementById('close1');
+    const close2 = document.getElementById('close2');
+    const close3 = document.getElementById('close3');
+
+    const hover_enable = () => window.innerWidth > 839; // Flag changes behaviour of hover effect for 'circle' buttons
+
+    dots.forEach(dot => dot.addEventListener('click', dotClick));
 
     showSlides();
 
     function showSlides() {
-        let i = null;
-        let slides = document.getElementsByClassName("image-sliderfade");
-        let dots = document.getElementsByClassName("dot");
+        const firstSliderNum = 1;
+        const slides = Array.prototype.slice.call(document.getElementsByClassName("image-sliderfade"));
+
+        slides.forEach(slide => {
+            slide.classList.remove('hide');
+
+            slide.style.display = 'none';
+        });
+
+        slider.expand?.removeEventListener('click', onExpand);
+
+        slider.collapse?.removeEventListener('click', onCollapse);
+
+        slider.current?.classList.toggle('hide');
+
+        slider.index = slider.index + 1 <= slides.length ? slider.index + 1 : firstSliderNum;
+
+        slider.image = slides[slider.index - 1];
+        slider.image.style.display = "flex";
         
-        for (i=0; i < slides.length; i++) {
-            if (slides[i].classList.contains('hide')) {
-                slides[i].classList.remove('hide');
+        slider.current = slider.image.children[0];
+        slider.current.classList.toggle('hide');
+
+        slider.img = slider.image.children[1];
+    
+        slider.span = slider.current.children[0];
+
+	    Array.prototype.slice.call(slider.current.children).forEach(child => {
+            if (child.className.includes('expand')) {
+                slider.expand=child;
+                slider.expand.addEventListener('click', onExpand);
+            } else if (child.className.includes('collapse')) {
+                slider.collapse = child;
+                slider.collapse.addEventListener('click', onCollapse);
+            } else if (child.className.includes('content')) {
+                slider.content = child;
             }
+         })
 
-            slides[i].style.display = 'none';
-        }
+        dots.forEach(dot => dot.className = dot.className.replace(" active", ""));
 
-        if (expand !== null) {
-            expand.removeEventListener('click', onExpand);
-        }
+        dots[slider.index - 1].className += " active";
 
-        if (collapse !== null) {
-            collapse.removeEventListener('click', onCollapse);
-        }
-
-        if (currSlider !== null) {
-            currSlider.classList.toggle('hide');
-        }
-
-        slideIndex++;
-
-        if (slideIndex > slides.length) {
-            slideIndex = 1;
-        }
-
-        currImage = slides[slideIndex-1];
-        currImage.style.display = "flex";
-        
-        currSlider = currImage.children[0];
-        currSlider.classList.toggle('hide');
-        img = currImage.children[1];
-
-        span = currSlider.children[0];
-
-        for (let i=0;i<currSlider.childElementCount;i++) {
-            if (currSlider.children[i].className.includes('expand')) {
-                expand=currSlider.children[i];
-                expand.addEventListener('click', onExpand);
-            } else if (currSlider.children[i].className.includes('collapse')) {
-                collapse = currSlider.children[i];
-                collapse.addEventListener('click', onCollapse);
-            } else if (currSlider.children[i].className.includes('content')) {
-                content = currSlider.children[i];
-            }
-         }
-
-        for (i=0; i<dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
-
-        dots[slideIndex-1].className += " active";
-
-        timer = setTimeout(showSlides, 5000);
+        slider.timer = setTimeout(showSlides, 5000);
     }
 
     function stopSlides() {
-        if (timer === null) {
+        if (!slider.timer) {
             return;
         }
-        
-        clearTimeout(timer);
-        
-        timer = null;
-        
-        slideIndex--;
+
+        clearTimeout(slider.timer);
+
+        slider.timer = null;
+
+        slider.index--;
     }
 
     function dotClick() {
-    
-        let num = this.id.replace('dot','');
-        
-        if (expand.className.includes('hide')) {
+        const num = this.id.replace('dot','');
+
+        if (slider.expand.className.includes('hide')) {
             onCollapse();
         }
 
         stopSlides();
-        
-        slideIndex = num-1;
-        
+
+        slider.index = num - 1;
+
         showSlides();
-    
     }
 
-    function rarrowClick() {
-
-        if (expand.className.includes('hide')) {
+    function rArrowClick() {
+        if (slider.expand.className.includes('hide')) {
             onCollapse();
         }
 
         stopSlides();
 
-        slideIndex++;
+        slider.index++;
 
         showSlides();
-    
     }
 
-    function larrowClick() {
-        
-        if (expand.className.includes('hide')) {
+    function lArrowClick() {
+        const lastSliderNumber = 3;
+
+        if (slider.expand.className.includes('hide')) {
             onCollapse();
         }
-        
-        stopSlides();
-        slideIndex--;
 
-        if (slideIndex < 0) slideIndex = 3;
+        stopSlides();
+        
+        slider.index = slider.index - 1 >= 0 ? slider.index - 1 : lastSliderNumber;
 
         showSlides();
-    
     }
 
     function setSliderHeight() {
-    
-        if (!expand.className.includes('hide')) {
-            currSlider.style.height = '';
-            currSlider.style.marginBottom = '';
+        if (!slider.expand.className.includes('hide')) {
+            slider.current.style.height = '';
+            slider.current.style.marginBottom = '';
         } else {
-            let height = img.offsetHeight;
-            currSlider.style.height = height+"px";
-            currSlider.style.marginBottom = -height+"px";
+            const height = slider.img.offsetHeight;
+
+            slider.current.style.height = height+"px";
+            slider.current.style.marginBottom = -height+"px";
         }
-    
     }
 
     function onExpand() {
-        
-        span.classList.toggle('hide');
-        expand.classList.toggle('hide');
-        collapse.classList.toggle('hide');
-        currImage.classList.remove('sbtn__expand');
-        currImage.classList.add('sbtn__collapse');
-        currSlider.classList.remove('sl__expand');
-        currSlider.classList.add('sl__collapse');
-        
+        slider.span.classList.toggle('hide');
+        slider.expand.classList.toggle('hide');
+        slider.collapse.classList.toggle('hide');
+        slider.image.classList.remove('sbtn__expand');
+        slider.image.classList.add('sbtn__collapse');
+        slider.current.classList.remove('sl__expand');
+        slider.current.classList.add('sl__collapse');
+
         setSliderHeight();
-        
-        content.classList.toggle('hide');
+
+        slider.content.classList.toggle('hide');
 
         stopSlides();
-    
     }
 
     function onCollapse() {
-        console.log('collapse');
-        span.classList.toggle('hide');
-        expand.classList.toggle('hide');
-        collapse.classList.toggle('hide');
-        currImage.classList.add('sbtn__expand');
-        currImage.classList.remove('sbtn__collapse');
-        currSlider.classList.add('sl__expand');
-        currSlider.classList.remove('sl__collapse');
-        
+        slider.span.classList.toggle('hide');
+        slider.expand.classList.toggle('hide');
+        slider.collapse.classList.toggle('hide');
+        slider.image.classList.add('sbtn__expand');
+        slider.image.classList.remove('sbtn__collapse');
+        slider.current.classList.add('sl__expand');
+        slider.current.classList.remove('sl__collapse');
+
         setSliderHeight();
-        
-        content.classList.toggle('hide');
+
+        slider.content.classList.toggle('hide');
 
         showSlides();
     }
 
-    larrow.addEventListener('click', larrowClick);
-    rarrow.addEventListener('click', rarrowClick);
+    lArrow.addEventListener('click', lArrowClick);
+    rArrow.addEventListener('click', rArrowClick);
 
     /* Protfolio part */
 
     function onMouseEnter(num) {
-
-        if (!hover_enable) {
+        if (!hover_enable()) {
             modal.classList.toggle('premodal');     
             modal.classList.toggle('modal');
             document.body.style.overflow="hidden";
@@ -226,13 +207,12 @@ window.onload = function() {
         }
 
         viewProject.classList.toggle('hide');
-        let project = viewProject.children[num];
-        showProject(project);
+
+        showProject(viewProject.children[num]);
     }
 
     function onMouseLeave(num) {
-
-        if (!hover_enable) {
+        if (!hover_enable()) {
             modal.classList.toggle('premodal');     
             modal.classList.toggle('modal');
             document.body.style.removeProperty('overflow');
@@ -240,120 +220,81 @@ window.onload = function() {
             viewProject.classList.add('view_project');
         }
 
-        let project = viewProject.children[num];
+        const project = viewProject.children[num];
+
         hideProject(project);
+
         viewProject.classList.toggle('hide');
- 
-    }
+     }
 
     function showProject(project) {
-        
         project.classList.toggle('hide');
         
-        if (!hover_enable) {
-            project.children[2].classList.toggle('hide');
-        }
-
+        hover_enable() || project.children[2].classList.toggle('hide');
     }
 
     function hideProject(project) {
         project.classList.toggle('hide');
         
-        if (!hover_enable) {
+        if (!hover_enable()) {
             project.children[2].classList.toggle('hide');
         }
     }
 
-    project1.onmouseenter = function(event) {
-        if (hover_enable) {
-            onMouseEnter(0);
-        }
+    project1.onmouseenter = () => {
+        hover_enable() && onMouseEnter(0);
     };
 
-    project1.onmouseleave = function(event) {
-        if (hover_enable) {
-            onMouseLeave(0);
-        }  
+    project1.onmouseleave = () => {
+        hover_enable() && onMouseLeave(0);
     };
 
-    project1.onclick = function(event) {
-        
-        if (hover_enable) {
-            window.open("https://lemadior.pp.ua", "_blank");
+    project1.onclick = () => {
+        if (hover_enable()) {
+            window.open(projectLinks.project1, "_blank");
         } else {
             onMouseEnter(0);
         }
-
     }
 
-    close1.onclick = function(event) {
-        onMouseLeave(0)
+    close1.onclick = () => onMouseLeave(0);
+
+    project2.onmouseenter = () => {
+        hover_enable() && onMouseEnter(1);
     }
 
-    project2.onmouseenter = function(event) {
-        if (hover_enable) {
-            onMouseEnter(1);
-        }
+    project2.onmouseleave = () => {
+        hover_enable() && onMouseLeave(1);
     }
 
-    project2.onmouseleave = function(event) {
-        if (hover_enable) {
-            onMouseLeave(1);
-        }
-    }
-
-    project2.onclick = function(event) {
-        if (hover_enable) {
-            window.open("https://lemadior.pp.ua/nikos/start.html", "_blank");
+    project2.onclick = () => {
+        if (hover_enable()) {
+            window.open(projectLinks.project2, "_blank");
         } else {
             onMouseEnter(1);
         }  
     }
 
-    close2.onclick = function(event) {
-        onMouseLeave(1)
-    }
+    close2.onclick = () => onMouseLeave(1);
 
-    project3.onmouseenter = function(event) {
-         if (hover_enable) {
-            onMouseEnter(2);
-        }
+    project3.onmouseenter = () => {
+         hover_enable() && onMouseEnter(2);
     };
 
-    project3.onmouseleave = function(event) {
-
-        if (hover_enable) onMouseLeave(2);
-
+    project3.onmouseleave = () => {
+        hover_enable() && onMouseLeave(2);
     };
 
-    project3.onclick = function(event) {
-        
-        if (hover_enable) {
-            window.open("https://olliplus.com.ua", "_blank");
+    project3.onclick = () => {
+        if (hover_enable()) {
+            window.open(projectLinks.project3, "_blank");
         } else {
             onMouseEnter(2);
         }
-
     }
 
-    close3.onclick = function(event) {
-
-        onMouseLeave(2)
-
-    }
+    close3.onclick = () => onMouseLeave(2);
 
     /* Global section */
-
-    window.addEventListener('resize', function(event){
-        
-        if (window.innerWidth <= 839) {
-            hover_enable = false;
-        } else {
-            hover_enable = true;
-        }
-        
-        setSliderHeight();
-
-    });
-
+    window.addEventListener('resize', setSliderHeight);
 }
